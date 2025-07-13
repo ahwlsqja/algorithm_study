9ff6008ddf15cf4ab4802085f3b29e8990b40d32
후니 <hipark@huniui-MacBookPro.local>
2025-07-13 23:20:38 +0900

    백준 1260: DFS와 BFS 풀이

---
+++ week2/1260.py
@@ -0,0 +1,37 @@
+# DFS와 BFS
+from collections import deque
+import sys
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in sorted(graph[v]):
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in sorted(graph[v]):
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+n, m, v = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n + 1)
+dfs(graph, v, visited)
+print()
+
+visited = [False] * (n + 1)
+bfs(graph, v, visited)
+print()
