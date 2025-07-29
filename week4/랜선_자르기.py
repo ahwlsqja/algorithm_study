545e4ce980631e718a1cb87ee7a7eb3f8659e580
유저이름 <ahwlsqja1324@gmail.com>
2025-07-30 01:08:31 +0900

    백준 1654: 랜선 자르기 풀이

---
+++ week4/랜선_자르기.py
@@ -0,0 +1,27 @@
+K, N = map(int, input().split())
+
+cables = []
+
+for _ in range(K):
+    cables.append(int(input()))
+
+left = 1
+right = max(cables)
+answer = 0
+
+while left <= right:
+    mid = (left + right) // 2
+
+    count = 0
+
+    for cable in cables:
+        count += cable // mid
+
+    if count >= N:
+        answer = mid
+        left = mid + 1
+    
+    else:
+        right = mid - 1
+
+print(answer)
