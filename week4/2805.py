a8b7d1d7877855ba6fe50e37e0ac8d7f8d3e8abc
후니 <hipark@huniui-MacBookPro.local>
2025-07-31 15:36:49 +0900

    백준 2805: 나무 자르기 풀이

---
+++ week4/2805.py
@@ -0,0 +1,24 @@
+# 나무 자르기
+import sys
+
+n, m = list(map(int, sys.stdin.readline().split(' ')))
+array = list(map(int, sys.stdin.readline().split()))
+
+
+start, end = 0, max(array)
+result = 0
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+
+    for x in array:
+        if x > mid:
+            total += x - mid
+    
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
